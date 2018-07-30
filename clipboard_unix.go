b25ebd2c2713b3a3f9d08eee37e088d8f161a87b
Javed Khan <tuxcanfly@gmail.com>
2018-07-30 13:36:41 +0530

    change

--- clipboard_unix.go
+++ clipboard_unix.go
@@ -107,59 +107,6 @@ func writeAll(text string) error {
 	return copyCmd.Wait()
 }
 
-func compressPropertyNotify(X *xgbutil.XUtil,
-	ev xevent.PropertyNotifyEvent) xevent.PropertyNotifyEvent {
-
-	// We force a round trip request so that we make sure to read all
-	// available events.
-	X.Sync()
-	xevent.Read(X, false)
-
-	// The most recent PropertyNotify event that we'll end up returning.
-	laste := ev
-
-	// Look through each event in the queue. If it's an event and it matches
-	// all the fields in 'ev' that are detailed above, then set it to 'laste'.
-	// In which case, we'll also dequeue the event, otherwise it will be
-	// processed twice!
-	// N.B. If our only goal was to find the most recent relevant PropertyNotify
-	// event, we could traverse the event queue backwards and simply use
-	// the first PropertyNotify we see. However, this could potentially leave
-	// other PropertyNotify events in the queue, which we *don't* want to be
-	// processed. So we stride along and just pick off PropertyNotify events
-	// until we don't see any more.
-	for i, ee := range xevent.Peek(X) {
-		if ee.Err != nil { // This is an error, skip it.
-			continue
-		}
-
-		// Use type assertion to make sure this is a PropertyNotify event.
-		if mn, ok := ee.Event.(xproto.PropertyNotifyEvent); ok {
-			// Now make sure all appropriate fields are equivalent.
-			if ev.Atom == mn.Atom && ev.Window == mn.Window {
-
-				// Set the most recent/valid motion notify event.
-				laste = xevent.PropertyNotifyEvent{&mn}
-
-				// We cheat and use the stack semantics of defer to dequeue
-				// most recent motion notify events first, so that the indices
-				// don't become invalid. (If we dequeued oldest first, we'd
-				// have to account for all future events shifting to the left
-				// by one.)
-				defer func(i int) { xevent.DequeueAt(X, i) }(i)
-			}
-		}
-	}
-
-	// This isn't strictly necessary, but is correct. We should update
-	// xgbutil's sense of time with the most recent event processed.
-	// This is typically done in the main event loop, but since we are
-	// subverting the main event loop, we should take care of it.
-	X.TimeSet(laste.Time)
-
-	return laste
-}
-
 func monitorAll(text chan<- string, quit <-chan struct{}) error {
 	X, err := xgbutil.NewConn()
 	if err != nil {
@@ -175,18 +122,38 @@ func monitorAll(text chan<- string, quit <-chan struct{}) error {
 		return err
 	}
 
+	events := make(chan xevent.PropertyNotifyEvent)
+	go func() {
+		var last uint32
+		var old string
+		for {
+			select {
+			case ev := <-events:
+				fmt.Println(uint32(ev.Time) - last)
+				if uint32(ev.Time)-last > 100 {
+					copy, err := readAll()
+					if err != nil {
+						continue
+					}
+					if copy != old {
+						text <- copy
+						old = copy
+					}
+				}
+				last = uint32(ev.Time)
+			case <-quit:
+				return
+			}
+		}
+	}()
+
 	// Respond to those X events.
 	xevent.PropertyNotifyFun(
 		func(X *xgbutil.XUtil, ev xevent.PropertyNotifyEvent) {
-			if ev.Atom != atomReply.Atom {
+			if ev.Atom != atomReply.Atom || ev.State != 1 {
 				return
 			}
-			if ev.State != 1 {
-				return
-			}
-			ev = compressPropertyNotify(X, ev)
-			fmt.Println(ev)
-			time.Sleep(workTime)
+			events <- ev
 		}).Connect(X, X.RootWin())
 
 	xevent.Main(X)
